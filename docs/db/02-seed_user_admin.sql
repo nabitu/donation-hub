43c8ce9b0f50484d438c26125bc3d0d780f31fa7
Robbyn Rahmandaru <robzlabz@gmail.com>
2024-05-26 22:55:20 +0700

    chore: add sample user

--- docs/db/02-seed_user_admin.sql
+++ docs/db/02-seed_user_admin.sql
@@ -1,7 +1,12 @@
 USE donation_hub;
-
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`)
-VALUES (1, 'admin', 'admin@donationhub.com', 'admin123', UNIX_TIMESTAMP());
+VALUES
+(1, 'admin', 'admin@donationhub.com', 'admin123', UNIX_TIMESTAMP()),
+(2, 'donor', 'donor@donationhub.com', 'donatur123', UNIX_TIMESTAMP()),
+(3, 'requester', 'requester@donationhub.com', 'requester123', UNIX_TIMESTAMP());
 
 INSERT INTO `user_roles` (`user_id`, `role`)
-VALUES (1, 'admin');
+VALUES
+(1, 'admin'),
+(2, 'donor'),
+(3, 'requester');
