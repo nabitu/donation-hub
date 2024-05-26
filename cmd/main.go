fd374a94059dde53dda4756e82f7bb867e534e73
fatkulnurk <fatkulnurk@gmail.com>
2024-05-26 16:38:03 +0700

    add initialization

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"github.com/isdzulqor/donation-hub/internal/driven/auth/jwt"
+	"github.com/isdzulqor/donation-hub/internal/driven/storage/mysql/projectstorage"
+	"github.com/isdzulqor/donation-hub/internal/driven/storage/mysql/userstorage"
+	"github.com/isdzulqor/donation-hub/internal/driven/storage/s3/projectfilestorage"
 	"log"
 	"net/http"
 )
 
 func main() {
+	c := InitContainer()
+	_ = userstorage.New(c)
+	_ = projectstorage.New(c)
+	_ = projectfilestorage.New(c)
+	_ = jwt.New(c)
+
 	log.Println("Starting server on :8180")
 	http.ListenAndServe(":8180", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
