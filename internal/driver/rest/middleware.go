791d2069115b65405e3ea7ca3b91e73213215ac8
robzlabz <robzlabz@gmail.com>
2024-05-30 15:20:58 +0700

    fix: add role to middleware

--- internal/driver/rest/middleware.go
+++ internal/driver/rest/middleware.go
@@ -2,12 +2,13 @@ package rest
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"strings"
 )
 
 // authTokenMiddleware middleware for checking Bearer token
-func authTokenMiddleware(next http.HandlerFunc, c *Config, isOptional bool) http.HandlerFunc {
+func authTokenMiddleware(next http.HandlerFunc, c *Config, isOptional bool, roles []string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 
@@ -32,6 +33,24 @@ func authTokenMiddleware(next http.HandlerFunc, c *Config, isOptional bool) http
 			if err != nil {
 				ResponseErrorInvalidAccessToken(w, "Invalid access token")
 				return
+			}
+
+			// jika roles tidak kosong, maka cek apakah role user ada di dalam roles
+			if len(roles) > 0 {
+				isRoleValid := false
+				for _, role := range roles {
+					for _, userRole := range payload.Role {
+						if role == userRole {
+							isRoleValid = true
+							break
+						}
+					}
+				}
+
+				if !isRoleValid {
+					ResponseErrorInvalidAccessToken(w, fmt.Sprintf("Invalid role, expected: %s", strings.Join(roles, ", ")))
+					return
+				}
 			}
 
 			ctx = context.WithValue(ctx, "auth_id", payload.UserID)
